642993c07877ddabd53c2d5e55953ebb427f4c2d
Yannick Leyendecker <drupal@yannickoo.de>
2012-07-27 17:06:45 +0200

    Updated linked_field module.

---
+++ linked_field.api.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * @file
+ * Hooks provided by the Linked Field module.
+ */
+
+/**
+ * Alter the supported field formatters.
+ * 
+ * This hook is invoked by linked_field_supported_formatters().
+ * 
+ */
+function hook_linked_field_supported_fields_alter(&$formatters) {
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
