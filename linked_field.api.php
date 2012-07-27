ec6d4483c319100ad2b6ad5861977196caaff295
Yannick Leyendecker <drupal@yannickoo.de>
2012-07-28 00:34:46 +0200

    Complied with coding standards.

--- linked_field.api.php
+++ linked_field.api.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * @addtogroup hooks
- * @{
- */
-
-/**
  * @file
  * Hooks provided by the Linked Field module.
  */
 
 /**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
  * Alter the supported field formatters.
- * 
+ *
  * This hook is invoked by linked_field_supported_formatters().
- * 
+ *
  */
 function hook_linked_field_supported_fields_alter(&$formatters) {
 
